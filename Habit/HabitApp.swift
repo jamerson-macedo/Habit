2f9fc0078ae7cbc1615d743c1b53a1660b9ca198
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-06-26 21:07:09 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Jamerson Macedo on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

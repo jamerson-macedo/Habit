159cbbb7fcf5bcf37e5202c6d01016b65172e186
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-07-19 01:01:32 -0300

    FINALIZANDO

--- Habit/HabitDetail/View/HabitDetailView.swift
+++ Habit/HabitDetail/View/HabitDetailView.swift
@@ -54,10 +54,10 @@ struct HabitDetailView: View {
         .padding(.horizontal,8)
         .padding(.top,32)
         .onAppear{
-            viewModel.$uiState.sink { uiState in
-                if uiState == .success{
-                    // se veio sucesso ai ele tira da principal
-                    self.presentationMode.wrappedValue.dismiss()
+            viewModel.$uiState.receive(on: DispatchQueue.main).sink { uiState in
+                if uiState == .success {
+                    print("Received uiState: \(uiState)")
+                  self.presentationMode.wrappedValue.dismiss()
                 }
             }.store(in: &viewModel.cancellables)
         }
